033ac53e4ee3f02003c94391381c1e840033ea32
normeow <normeow@yandex.ru>
2017-11-06 21:53:02 +0300

    solve dp: infinite coins

--- ProblemsSolving/DP/infinite_coins.py
+++ ProblemsSolving/DP/infinite_coins.py
@@ -3,7 +3,7 @@ Given:
 coins a1, a2 ... an
 sum S > 0
 
-Can we collect sum s from coins using each only once?
+Can we collect sum s from coins if each coin is available in an infinite quantity?
 
 '''
 
@@ -11,15 +11,22 @@ Can we collect sum s from coins using each only once?
 def solution(a, s):
     coins_num = len(a)
     ans_matrix = [[0] * (s + 1) for _ in range(coins_num)]
-    ans_matrix[0][0] = 1
-    ans_matrix[0][a[0]] = 1
+
+    # from first coin we can collect any sum that is divisible by coin
+    for i in range(s + 1):
+        if i % a[0] == 0:
+            ans_matrix[0][i] = 1
+
     for coin_i in range(1, coins_num):
         for sub_s in range(s+1):
-            ans_matrix[coin_i][sub_s] = ans_matrix[coin_i - 1][sub_s] or ans_matrix[coin_i - 1][sub_s - a[coin_i]]
+            ans_matrix[coin_i][sub_s] = ans_matrix[coin_i - 1][sub_s]
+            temp_s = sub_s
+            while temp_s >= a[coin_i]:
+                temp_s -= a[coin_i]
+                ans_matrix[coin_i][sub_s] = ans_matrix[coin_i][sub_s] or ans_matrix[coin_i - 1][temp_s]
 
     for i in range(coins_num):
         if ans_matrix[i][s]:
             return True
 
     return False
-
